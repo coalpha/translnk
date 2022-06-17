bd50da783da88f70f7d5e1a03418a0d43d44a835
coalpha <22552895+coalpha@users.noreply.github.com>
2022-06-16 17:55:05 -0700

    Db
    C# is not very fun

---
+++ QZN.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using Shell32;
+using System.IO;
+using System.Text;
+using System.Diagnostics;
+using System.Windows.Forms;
+
+namespace translnkte
+{
+    internal class QZN
+    {
+        static string resolve_lnk(string lnk)
+        {
+            Shell s = new Shell();
+            var filename = Path.GetFileName(lnk);
+            var dirname  = Path.GetDirectoryName(lnk);
+            var folder   = s.NameSpace(dirname);
+            if (folder == null)
+            {
+                return lnk;
+            }
+            var item = folder.ParseName(filename);
+            if (item == null)
+            {
+                return lnk;
+            }
+            var link = (Shell32.ShellLinkObject) item.GetLink;
+            return link.Target.Path;
+        }
+        [STAThread]
+        static void Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                MessageBox.Show("translnkte.exe program [...args]", "Incorrect Usage", MessageBoxButtons.OK, MessageBoxIcon.Stop);
+                return;
+            }
+            var ps = new Process();
+            ps.StartInfo.FileName = args[0];
+            var sb = new StringBuilder(0xff);
+            for (int i = 1; i < args.Length; i++)
+            {
+                var arg = args[i];
+                if (arg.EndsWith(".lnk"))
+                {
+                    arg = resolve_lnk(arg);
+                }
+                foreach (char c in arg)
+                {
+                    if (c == '\\')
+                    {
+                        sb.Append("\\\\");
+                        continue;
+                    }
+                    if (c == '"')
+                    {
+                        sb.Append("\\\"");
+                        continue;
+                    }
+                    if (c == ' ')
+                    {
+                        sb.Append("\\ ");
+                        continue;
+                    }
+                    sb.Append(c);
+                }
+                if (i + 1 < args.Length)
+                {
+                    sb.Append(" ");
+                }
+            }
+            ps.StartInfo.Arguments = sb.ToString();
+            ps.Start();
+            ps.WaitForExit();
+            Environment.Exit(ps.ExitCode);
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,31 @@
+# translnkte
+
+*Pronounced "translate"*
+
+To start programming, I like to drag the repositories that I have on my desktop
+to a vscode shortcut.
+
+![](misc/drag.png)
+
+When doing that, it executes something like this:
+
+```
+"C:/Program Files/vscode/Code.exe" C:/Users/Me/Desktop/coalpha
+```
+
+So this is fine for folders but it doesn't work really well for shortcut files
+because vscode will open the shortcut file instead of it's destination. This is
+correct behavior and I wouldn't want it to do anything else, but it's also not
+my intention.
+
+So what if we made a stub shortcut that targets something like this which was
+able to resolve the `.lnk` targets before passing them onto the actual
+application we want to run?
+
+```
+translnkte.exe C:/Program Files/vscode/Code.exe C:/Users/Me/Desktop/repo.lnk
+```
+
+Well, that's what this is. You still have to make the shortcuts yourself but at
+least I provide the `translnkte.exe`. The code for this is awful and I don't
+really care. All I need it to do is work.
