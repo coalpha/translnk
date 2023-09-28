04fdb8c88170e8f40e87622c45e750d3a289a15e
coalpha <22552895+coalpha@users.noreply.github.com>
2023-09-28 01:57:30 -0700

    update

--- README.md
+++ README.md
@@ -1,6 +1,4 @@
-# translnkte
-
-*Pronounced "translinktea"*
+# translnk
 
 To start programming, I like to drag the repositories that I have on my desktop
 to a vscode shortcut.
@@ -13,19 +11,8 @@ When doing that, it executes something like this:
 "C:/Program Files/vscode/Code.exe" C:/Users/Me/Desktop/coalpha
 ```
 
-So this is fine for folders but it doesn't work really well for shortcut files
-because vscode will open the shortcut file instead of it's destination. This is
-correct behavior and I wouldn't want it to do anything else, but it's also not
-my intention.
-
-So what if we made a stub shortcut that targets something like this which was
-able to resolve the `.lnk` targets before passing them onto the actual
-application we want to run?
+This workflow doesn't work for shortcut files (.lnk) since vscode opens the file itself instead of it's destination. Instead of targeting the application directly, change your shortcut to call `translnk` first. e.g:
 
 ```
-translnkte.exe C:/Program Files/vscode/Code.exe C:/Users/Me/Desktop/repo.lnk
+translnk C:/Program Files/vscode/Code.exe
 ```
-
-Well, that's what this is. You still have to make the shortcuts yourself but at
-least I provide the `translnkte.exe`. The code for this is awful and I don't
-really care. All I need it to do is work.
---
+++ Translnk.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using Shell32;
+using System.IO;
+using System.Text;
+using System.Diagnostics;
+using System.Windows.Forms;
+
+internal class Translnkte
+{
+    static string ResolveLink(string lnk)
+    {
+        Shell s = new Shell();
+        var filename = Path.GetFileName(lnk);
+        var dirname = Path.GetDirectoryName(lnk);
+        var folder = s.NameSpace(dirname);
+        if (folder == null)
+        {
+            return lnk;
+        }
+        var item = folder.ParseName(filename);
+        if (item == null)
+        {
+            return lnk;
+        }
+        var link = (Shell32.ShellLinkObject)item.GetLink;
+        return link.Target.Path;
+    }
+    // I have no idea what this does
+    // I think it has something to do with Winforms?
+    [STAThread]
+    static void Main(string[] args)
+    {
+        if (args.Length == 0)
+        {
+            MessageBox.Show("translnk program [...args]", "Incorrect Usage", MessageBoxButtons.OK, MessageBoxIcon.Stop);
+            return;
+        }
+        var ps = new Process();
+        ps.StartInfo.FileName = args[0];
+        var sb = new StringBuilder(0xff);
+        for (int i = 1; i < args.Length; i++)
+        {
+            var arg = args[i];
+            if (arg.EndsWith(".lnk"))
+            {
+                arg = ResolveLink(arg);
+            }
+            sb.Append('"');
+            foreach (char c in arg)
+            {
+                if (c == '\\')
+                {
+                    sb.Append("\\\\");
+                    continue;
+                }
+                if (c == '"')
+                {
+                    sb.Append("\\\"");
+                    continue;
+                }
+                sb.Append(c);
+            }
+            sb.Append('"');
+            // put spaces between arguments, not after
+            if (i + 1 < args.Length)
+            {
+                sb.Append(' ');
+            }
+        }
+        ps.StartInfo.Arguments = sb.ToString();
+        ps.Start();
+        ps.WaitForExit();
+        Environment.Exit(ps.ExitCode);
+    }
+}
